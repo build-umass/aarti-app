e251292190f8f3c4af6f83b5b14717ca8c27bf35
Roman Pisani <romanp2929@gmail.com>
2024-10-13 16:53:08 -0400

    removed unnecessary example code and changed icon color to pink

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -17,7 +17,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title:"",
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
           ),
@@ -26,7 +26,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title:"",
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
           ),
