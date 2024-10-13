35be4f0b87989ec80ada14693f071bb4209b85e3
Roman Pisani <romanp2929@gmail.com>
2024-10-13 15:44:31 -0400

    added bottom navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,21 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { NavigationContainer } from "@react-navigation/native";
+import { Stack } from "expo-router";
+import BottomNavigation from "./components/BottomNavigation";
+import { useState } from "react";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+const RootLayout: React.FC = () => {
+  const [activeTab, setActiveTab] = useState('Home');
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
+  const handleTabPress = (tabName: string) => {
+    setActiveTab(tabName);
+  };
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="index" />
+      <BottomNavigation activeTab={activeTab} onTabPress={handleTabPress} />
+    </Stack>
   );
 }
