6581d1c57b9921ee11c1cb530c4991854b1a7b04
Sean O'Dea <60398405+sodea9@users.noreply.github.com>
2025-02-25 18:23:35 -0500

    basic splash screen customization

--- apps/mobile_client/app/_layout.tsx
+++ apps/mobile_client/app/_layout.tsx
@@ -2,13 +2,14 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
