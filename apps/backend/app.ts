e5156c89ad3ace2a8a76ca4121fd1637df9f3cd8
Roman Pisani <romanp2929@gmail.com>
2025-03-01 14:21:39 -0500

    broken backend

---
+++ apps/backend/app.ts
@@ -0,0 +1,21 @@
+// apps/user-service/src/app.ts
+import express from 'express';
+import userRoutes from './routes/routes';
+
+export function createApp() {
+  const app = express();
+
+  // JSON parsing
+  app.use(express.json());
+  app.use((req, _, next) => {
+    console.log(`User-service incoming: ${req.method} ${req.url}`);
+    console.log(req.body)
+    next();
+  });
+  
+
+  // Register routes for user
+  app.use('/users', userRoutes);
+
+  return app;
+}
