153b0e9c4ed06e24fc9552caf0b2cb81f7b10f47
Sean O'Dea <60398405+sodea9@users.noreply.github.com>
2025-04-13 18:03:33 -0400

    added settings placeholder and stats layout

--- apps/mobile_client/app/(tabs)/profile.tsx
+++ apps/mobile_client/app/(tabs)/profile.tsx
@@ -1,30 +1,70 @@
-import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+import { StyleSheet, Pressable, View, Text, } from 'react-native';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function ProfileScreen() {
   return (
     <View style={styles.container}>
+
+      <Pressable style={styles.settings}>
+        <FontAwesome name="gear" size={40} color="black" />
+      </Pressable>
+
       <View style={styles.user}>
         <FontAwesome name="user-circle" size={170} color="black" />
         <Text style={styles.text}>Sean</Text>
+      </View>
+
+      <View style={styles.statsSelection}>
+
+        <Pressable style={styles.button}>
+          <Text>Quizzes</Text>  
+        </Pressable>
+
+        <View style={styles.divider}></View>
+
+        <Pressable style={styles.button}>
+          <Text>Resources</Text>  
+        </Pressable>
+
       </View>
     </View>
   );
 }
 
+const changeTabToQuizzes = () => {}
+const changeTabToResources = () => {}
+
 const styles = StyleSheet.create({
+  settings: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginRight: 10,
+    marginTop: 10,
+  },
   container: {
     flex: 1,
     backgroundColor: 'white',
-    alignItems: 'center',
   },
   user: {
-    marginTop: 60,
+    marginTop: 10,
     alignItems: 'center'
   },
   text: {
     fontWeight: 'bold',
     fontSize: 35,
     marginTop: 25,
+  },
+  statsSelection: {
+    justifyContent: 'center',
+    marginTop: 30,
+    flexDirection: 'row',
+  },
+  button: {
+
+  },
+  divider: {
+    width: 2,
+    height: '100%',
+    backgroundColor: '#ccc',
   },
 });
