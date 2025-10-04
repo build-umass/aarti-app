17271c9f6c4fa098838b517314511bc2667c3262
Riddhimaan-Senapati <rsenapati@umass.edu>
2025-10-04 11:35:38 -0400

    Updated the chatbot page to use react-native-safe-area-context as recommended by the documentation

--- apps/mobile_client/app/(tabs)/chatbot.tsx
+++ apps/mobile_client/app/(tabs)/chatbot.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import ChatScreen from '../../components/ChatScreen';
 
 export default function ChatBotScreen() {
