6269d76cbd81a78049e834353b5c43b180cc3e35
Riddhimaan-Senapati <rsenapati@umass.edu>
2025-11-23 16:33:49 -0500

    created new constants directory to keep track of colors used in App, also changed progress bar color to be uniformly purple

--- apps/mobile_client/constants/Colors.ts
+++ apps/mobile_client/constants/Colors.ts
@@ -3,7 +3,9 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#EE628C';
+import { BrandColors } from './Theme';
+
+const tintColorLight = BrandColors.primary; // Purple color used throughout the app
 const tintColorDark = '#fff';
 
 export const Colors = {
